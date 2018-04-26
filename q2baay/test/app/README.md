3e873af1c28f3c5098129c50e6e5e6e13ac5b439
wangyang <768188667@qq.com>
2018-04-26 15:02:47 +0800

    no -- middleware

--- q2baay/test/app/README.md
+++ q2baay/test/app/README.md
@@ -3,3 +3,12 @@
 #redux
 react-redux
 redux-thunk作为中间件
+
+本master分支没有middleware处理异步函数
+以及没有call - api的一个整合以及约定
+
+————在不涉及中间件的情况下，在master进行配置更改优化后合并入带有中间件的分支
+-
+-（待优化）按需加载bundle,react-router-dom官方已出新方案
+
+# v1/thunk-api分支，整合一个thunk的中间件
--- q2baay/test/app/redux/store.js
+++ q2baay/test/app/redux/store.js
@@ -4,6 +4,6 @@ import combineReducers from './reducers.js'
 // Logger with default options
 import logger from 'redux-logger'
 
-let store = (process.env.NODE_ENV === 'development') ? createStore(combineReducers, applyMiddleware(thunkMiddleware,logger)) : createStore(combineReducers, applyMiddleware(thunkMiddleware))
+let store = (process.env.NODE_ENV === 'development') ? createStore(combineReducers, applyMiddleware(logger)) : createStore(combineReducers, applyMiddleware())
 
 export default store
