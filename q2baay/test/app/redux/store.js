3cd525759c195c48e0cef1366069149e65d1fd81
wangyang <768188667@qq.com>
2018-04-26 23:22:30 +0800

    redux thunk to app

--- q2baay/test/app/redux/store.js
+++ q2baay/test/app/redux/store.js
@@ -1,9 +1,12 @@
-import {createStore, applyMiddleware} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import combineReducers from './reducers.js'
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import combineReducers from "./reducers.js";
 // Logger with default options
-import logger from 'redux-logger'
+import logger from "redux-logger";
 
-let store = (process.env.NODE_ENV === 'development') ? createStore(combineReducers, applyMiddleware(logger)) : createStore(combineReducers, applyMiddleware())
+let store =
+  process.env.NODE_ENV === "development"
+    ? createStore(combineReducers, applyMiddleware(thunkMiddleware, logger))
+    : createStore(combineReducers, applyMiddleware(thunkMiddleware));
 
-export default store
+export default store;
