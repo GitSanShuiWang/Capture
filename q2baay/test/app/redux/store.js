34d3413aa747212ba2a960db421183aa1a7bae0e
sanshuiwang <wangyangwy0815@163.com>
2018-05-14 14:58:03 +0800

    Update store.js

--- q2baay/test/app/redux/store.js
+++ q2baay/test/app/redux/store.js
@@ -4,8 +4,7 @@ import combineReducers from "./reducers.js";
 // Logger with default options
 import logger from "redux-logger";
 
-let store =
-  process.env.NODE_ENV === "development"
+let store = process.env.NODE_ENV === "development"
     ? createStore(combineReducers, applyMiddleware(thunkMiddleware, logger))
     : createStore(combineReducers, applyMiddleware(thunkMiddleware));
 
