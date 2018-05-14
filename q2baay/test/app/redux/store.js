65e2baffea7a19d50f1de5b87980805d63c02289
wangyang <768188667@qq.com>
2018-05-14 17:19:22 +0800

    use thunk to surport async func

--- q2baay/test/app/redux/store.js
+++ q2baay/test/app/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import combineReducers from "./reducers.js";
+import thunk from 'redux-thunk'
 // Logger with default options
 import logger from "redux-logger";
 
 let store = process.env.NODE_ENV === "development"
-    ? createStore(combineReducers, applyMiddleware(logger))
-    : createStore(combineReducers, applyMiddleware());
+    ? createStore(combineReducers, applyMiddleware(thunk,logger))
+    : createStore(combineReducers, applyMiddleware(thunk));
 
 export default store;
