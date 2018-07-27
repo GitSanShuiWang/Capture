fff5e95c7f44de260bc0756c833aa38ce652daf9
wangyang <768188667@qq.com>
2018-07-27 16:31:55 +0800

    package update & config too update

--- q2baay/test/webpack/webpack.production.config.js
+++ q2baay/test/webpack/webpack.production.config.js
@@ -105,24 +105,25 @@ module.exports = {
           {
             loader: "image-webpack-loader",
             options: {
-              gifsicle: {
-                interlaced: false
-              },
-              optipng: {
-                optimizationLevel: 7
-              },
-              pngquant: {
-                quality: "65-90",
-                speed: 4
-              },
               mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // Specifying webp here will create a WEBP version of your JPG/PNG images
-              webp: {
-                quality: 75
-              }
+               progressive: true,
+               quality: 65
+             },
+             // optipng.enabled: false will disable optipng
+             optipng: {
+               enabled: false,
+             },
+             pngquant: {
+               quality: '65-90',
+               speed: 4
+             },
+             gifsicle: {
+               interlaced: false,
+             },
+             // the webp option will enable WEBP
+             webp: {
+               quality: 75
+             }
             }
           }
         ]
